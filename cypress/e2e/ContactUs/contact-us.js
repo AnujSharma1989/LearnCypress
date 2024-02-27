0693b2d0933f3e1b6dbb561a1acaab6c2268849b
Anuj Sharma <anujsharma200789@gmail.com>
2024-02-27 14:15:47 +0530

    All Data

---
+++ cypress/e2e/ContactUs/contact-us.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+describe('Test case for contact us form', () => {
+    it('should submit the form', () => {
+        
+       cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html') 
+       //cy.get('#contact-us').click({force:true}) 
+       cy.get('[name="first_name"]').type("Anuj")
+       cy.get('[name="last_name"]').type("Sharma")
+       cy.get('[name="email"]').type("test@gmail.com")
+       cy.get('textarea.feedback-input').type("Sample text entered")
+       cy.get ('[type="Submit"]').click({force:true})
+    });
+    it('should not submit the form', () => {
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html') 
+        cy.get ('[type="Submit"]').click()
+        
+    });
+}); 
+
+describe('Test case for contact us' ,() => {
+    it ("Open contact us" ,() =>  {
+
+    })
+
+});
