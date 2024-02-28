47494a45b870e85ebc62c3986d70976c13b7964c
Anuj Sharma <anujsharma200789@gmail.com>
2024-02-28 18:55:05 +0530

    New file

--- cypress/e2e/ContactUs/automation-test-store/contact-us.js
+++ cypress/e2e/ContactUs/automation-test-store/contact-us.js
@@ -2,7 +2,8 @@
 
 describe("Contact us form from automation store" , ()=>{
     it("sucessful submission" , ()=>{
-        cy.visit("")
+        cy.visit("https://www.automationteststore.com/")
+        cy.get('.info_links_footer > :nth-child(5) > a').click()
 
     })
 
